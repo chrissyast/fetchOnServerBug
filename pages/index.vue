e769ffb92763be0763cf840bbab0358bc0505de1
Chris <cjastles@gmail.com>
2022-05-26 23:34:25 +0100

    additional comment

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,10 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  fetchOnServer () : boolean {
+  /*fetchOnServer () : boolean {
     return false
-  },
+  },*/ // same error whichever syntax is used
+  fetchOnServer: false,
   data () : any {
     return {
 
